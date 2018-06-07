d490098e4bdbfc93e120436cfaa2d6d50a4f04f5
Olufemi D <olufemidokun@hotmail.co.uk>
2018-06-07 22:28:59 +0100

    working on registration and confirmation email

--- ubiquity/registration/models.py
+++ ubiquity/registration/models.py
@@ -1,8 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
 class User(models.Model):
     userName = models.CharField(max_length=50)
-    email = models.EmailField()
-    password = models.CharField(max_length=50)
+    email_confirmed = models.BooleanField(default=False)
+    # other fields...
+
+
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
