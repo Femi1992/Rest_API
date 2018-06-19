333c7c06cd912afc58b71d65e39ad3919d2458e7
Olufemi D <olufemidokun@hotmail.co.uk>
2018-06-19 12:56:41 +0100

    issue sending confirmation email

--- ubiquity/registration/views.py
+++ ubiquity/registration/views.py
@@ -1,27 +1,28 @@
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-from .forms import SignupForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.contrib.sites.shortcuts import get_current_site
+from django.shortcuts import render, redirect
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.template.loader import render_to_string
-from .tokens import account_activation_token
-from django.contrib.auth.models import User
-from django.core.mail import EmailMessage
+from.forms import SignupForm
+from.tokens import account_activation_token
+
+
 
 def home(request):
-    return render(request,'home.html')
+    return render(request, 'home.html')
 
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
+            user.is_active = False
             user.save()
-
-            current_site = get_current_site()
-            subject = 'Activate your mysite account'
+            current_site = get_current_site(request)
+            subject = 'Activate Your MySite Account'
             message = render_to_string('account_activation_email.html', {
                 'user': user,
                 'domain': current_site.domain,
@@ -34,18 +35,22 @@ def signup(request):
         form = SignupForm()
     return render(request, 'signup.html', {'form': form})
 
+def account_activation_sent(request):
+    return render(request, 'account_activation_sent.html')
+
 
 def activate(request, uidb64, token):
     try:
         uid = force_text(urlsafe_base64_decode(uidb64))
         user = User.objects.get(pk=uid)
-    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
         user = None
+
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
+        user.profile.email_confirmed = True
         user.save()
         login(request, user)
-        # return redirect('home')
-        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+        return redirect('home')
     else:
-        return HttpResponse('Activation link is invalid!')
+        return render(request, 'account_activation_invalid.html')
